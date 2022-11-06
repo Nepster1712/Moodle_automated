a33d47d94a258a3f580f592ded6b5419535ba6b6
Florian Werner <florian.b.werner@t-online.de>
2022-11-06 13:40:00 +0100

    Change Logging

--- main.py
+++ main.py
@@ -7,22 +7,28 @@ from datetime import datetime
 
 def init():
     print("Intialization application for moodle export")
-    #_current_date = datetime.today().strftime('%Y%m%d')
     _filename = 'log_moodle_automated.log'
     try:
         logging.basicConfig(filename=_filename, encoding='utf-8', level=logging.DEBUG)
     except FileExistsError:
         logging.info("Log file " + _filename + " already exists.")
         pass
+    finally:
+        del _filename
+
+# Main function
 def main():
     # Init application
     init()
 
     try:
-        os.makedirs('parentdirectory/mydirectory')
+        _dirName = 'Test'
+        os.makedirs(_dirName)
     except FileExistsError:
-        logging.error("Directory was not able do be created.")
+        logging.error(datetime.today().strftime('[%Y-%m-%d %H:%M:%S]') + ' Directory ' + _dirName + ' was not able do be created.')
         pass
+    finally:
+        del _dirName
 
 
 if __name__ == '__main__':
