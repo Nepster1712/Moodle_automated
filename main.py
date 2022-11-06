737f8b1e064258d5875ac2a7d34677b2cc2f8bac
Florian Werner <florian.b.werner@t-online.de>
2022-11-06 13:26:21 +0100

    Create logging

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 # Main file for automation of Moodle synchonization
 
+import os
+import logging
+from datetime import datetime
+
+
+def init():
+    print("Intialization application for moodle export")
+    #_current_date = datetime.today().strftime('%Y%m%d')
+    _filename = 'log_moodle_automated.log'
+    try:
+        logging.basicConfig(filename=_filename, encoding='utf-8', level=logging.DEBUG)
+    except FileExistsError:
+        logging.info("Log file " + _filename + " already exists.")
+        pass
 def main():
-    print("python main function")
+    # Init application
+    init()
+
+    try:
+        os.makedirs('parentdirectory/mydirectory')
+    except FileExistsError:
+        logging.error("Directory was not able do be created.")
+        pass
 
 
 if __name__ == '__main__':
